f7192476e58d2c18678352aacadfd87a5b0d2d5e
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-05-11 15:00:21 +0200

    add fetch_s3 script

---
+++ fetch_s3.py
@@ -0,0 +1,37 @@
+import os
+import io
+import boto3
+import sys
+ 
+ 
+# Initializing variables for the client
+S3_BUCKET_NAME = "maes-bucket"  #Fill this in 
+S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
+S3_SECRET_ACCESS_KEY = os.environ.get('S3_SECRET_ACCESS_KEY')
+S3_ENDPOINT_URL = "https://storage.ecmwf.europeanweather.cloud"  #Fill this in
+
+class S3Connect:
+    def __init__(self) -> None:
+        self.s3 = boto3.client(
+            "s3",
+            endpoint_url=S3_ENDPOINT_URL,
+            aws_access_key_id=S3_ACCESS_KEY,
+            aws_secret_access_key=S3_SECRET_ACCESS_KEY
+        )
+
+    def fetch_file(self, file_path):
+        # Downloading a file from the bucket
+        try:
+            with open(file_path, "wb") as f:
+                self.s3.download_fileobj(
+                    S3_BUCKET_NAME, file_path, f)
+        except FileNotFoundError:
+            print("File not found")
+
+def main():
+    s3_file_path = sys.argv[1]
+    S3Connect().fetch_file(s3_file_path)
+
+
+if __name__ == "__main__":
+    main()
