3c6b786cc10760d1dd3ec4574e7e3760a7e6fe49
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-06-13 01:39:27 +0200

    logging changes

--- skinnywms_trigger.sh
+++ skinnywms_trigger.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Get command line argument
-data_loc=$1
+DATA_LOC=$1
 
 # Source the environment file
 source .env
 export LOG_FILE_PATH="$LOG_DIR/skinnywms_trigger.log"
 
+# Logging start
+echo "===========================================================================================================================" >> $LOG_FILE_PATH
+
 # Fetch s3 file
-echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - SkinnyWMSTrigger - Fetching '$data_loc' from s3 bucket" >> fetch_s3.log
-python s3_file_fetcher.py $data_loc
+echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - SkinnyWMSTrigger - Fetching '$DATA_LOC' from s3 bucket" >> fetch_s3.log
+python s3_file_fetcher.py $DATA_LOC
 
 # Check if file was downloaded and move it to skinnywms data dir
-if [ -f "$data_loc" ]; then
-  mv "$data_loc" "$HOME/data"
-  echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - SkinnyWMSTrigger - '$data_loc' was found and moved into to SkinnyWMS data dir at '$HOME/data'" >> $LOG_FILE_PATH
+if [ -f "$DATA_LOC" ]; then
+  mv "$DATA_LOC" "$HOME/data"
+  echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - SkinnyWMSTrigger - '$DATA_LOC' was found and moved into to SkinnyWMS data dir at '$HOME/data'" >> $LOG_FILE_PATH
 else
-  echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR - SkinnyWMSTrigger - '$data_loc' file does not exist. Terminating." >> $LOG_FILE_PATH
+  echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR - SkinnyWMSTrigger - '$DATA_LOC' file does not exist. Terminating." >> $LOG_FILE_PATH
   exit 1
 fi
 
@@ -32,3 +35,5 @@ if [ -n "$SERVICE_STATUS" ]; then
 else
     echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR - SkinnyWMSTrigger - Service '$SERVICE_NAME' is not running." >> $LOG_FILE_PATH
 fi
+
+echo "$separator" >> $LOG_FILE_PATH
