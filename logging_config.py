429458f0fe76401735dfdbdcde6ea60df994091e
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-08-24 18:10:38 +0200

    fixes

--- logging_config.py
+++ logging_config.py
@@ -7,9 +7,10 @@ dotenv_path = os.path.join(os.getenv("HOME"), ".env")
 load_dotenv(dotenv_path)
 
 log_file_path = os.getenv('DATAVISOR_LOG_PATH')
+trigger_conf_dir = os.getenv('DATAVISOR_TRIGGER_CONF_DIR')
 
 #Load the logging configuration from the YAML file
-with open('/opt/datavisor/trigger/logging.yaml', 'r') as file:
+with open(f'{trigger_conf_dir}/logging.yaml', 'r') as file:
     config = yaml.safe_load(file)
 # Replace the variable placeholder in the configuration dictionary
 config['handlers']['error_file']['filename'] = log_file_path
