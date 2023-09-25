4555eb0704b11a62d186eb09a2637526787869c1
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-09-25 11:32:37 +0200

    change send logs from bash to python

--- send_logs.py
+++ send_logs.py
@@ -9,7 +9,6 @@ from logging_config import logger
 dotenv_path = os.path.join(os.getenv("HOME"), ".env")
 load_dotenv(dotenv_path)
 
-# Load env file with credentials for S3 bucket
 DATAVISOR_SERVER_URL = os.getenv('DATAVISOR_SERVER_URL')
 VM_UUID = os.getenv('VM_UUID')
 DATAVISOR_LOG_PATH = os.getenv('DATAVISOR_LOG_PATH')
@@ -18,21 +17,17 @@ DATAVISOR_LOG_PATH = os.getenv('DATAVISOR_LOG_PATH')
 
 API_URL=f"{DATAVISOR_SERVER_URL}/api/v1/vms/{VM_UUID}/push-logs"
 
-# Read the content of the file into a variable
 with open(DATAVISOR_LOG_PATH, "r") as file:
     logs_content = file.read()
 
-# Construct the JSON payload with the content included in the "logs" key
 json_data = {
     "vm_uuid_id": VM_UUID,
     "logs": logs_content,
 }
 
-# Send the PATCH request with the JSON payload
 headers = {"Content-Type": "application/json", "X-Application-Key": APPLICATION_KEY}
 response = requests.patch(API_URL, json=json_data, headers=headers)
 
-# Check the HTTP status code
 if response.status_code == 200:
     logger.info("PATCH request successful")
 else:
