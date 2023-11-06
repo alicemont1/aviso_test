8a025b1e0debd14cd46cd84f4585debedabe01b2
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-11-06 20:45:58 +0100

    breaking changes

--- send_logs.py
+++ send_logs.py
@@ -9,24 +9,35 @@ load_dotenv(dotenv_path)
 DATAVISOR_SERVER_URL = os.getenv('DATAVISOR_SERVER_URL')
 VM_UUID = os.getenv('VM_UUID')
 DATAVISOR_LOG_PATH = os.getenv('DATAVISOR_LOG_PATH')
+DATAVISOR_LOG_PATH_BKP = os.getenv('DATAVISOR_LOG_PATH_BKP')
 APPLICATION_KEY = os.getenv('APPLICATION_KEY')
 
 API_URL=f"{DATAVISOR_SERVER_URL}/api/v1/vms/{VM_UUID}/push-logs"
 
+
+def backup_log(log_content):
+    with open(DATAVISOR_LOG_PATH_BKP, 'a') as target_file:
+        target_file.write(log_content)
+
 with open(DATAVISOR_LOG_PATH, "r") as file:
-    logs_content = file.read()
+    log_content = file.read()
+    json_data = {
+        "vm_uuid_id": VM_UUID,
+        "logs": log_content,
+    }
+    headers = {"Content-Type": "application/json", "X-Application-Key": APPLICATION_KEY}
+    response = requests.patch(API_URL, json=json_data, headers=headers)
 
-json_data = {
-    "vm_uuid_id": VM_UUID,
-    "logs": logs_content,
-}
+    if response.status_code == 200:
+        logger.info("Log PATCH request successful")
+        file.write("") # clear content in the file
+        backup_log(log_content)
+    else:
+        logger.error(f"Log PATCH request failed with status code: {response.status_code}")
+        logger.error(response.text)
 
-headers = {"Content-Type": "application/json", "X-Application-Key": APPLICATION_KEY}
-response = requests.patch(API_URL, json=json_data, headers=headers)
 
-if response.status_code == 200:
-    logger.info("PATCH request successful")
-else:
-    logger.error(f"PATCH request failed with status code: {response.status_code}")
-    logger.error(response.text)
+
+
+
 
