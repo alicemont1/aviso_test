bd97a363fdd4f6e5984a10a734f7d9277d9bbb84
maes <alice.sartorimonteirodebarros@ecmwf.int>
2023-11-09 11:48:15 +0100

    modify logging

--- skinnywms_trigger.sh
+++ skinnywms_trigger.sh
@@ -13,14 +13,14 @@ echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - $task_name - '$FILENAME' is the file
 
 # Fetch s3 file
 echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - $task_name - Fetching '$DATA_LOC' from s3 bucket" >> $DATAVISOR_LOG_PATH
-python s3_file_fetcher.py $DATA_LOC
+python $DATAVISOR_TRIGGER_CONF_DIR/s3_file_fetcher.py $DATA_LOC
 
 # Check if file was downloaded and move it to skinnywms data dir
 if [ -f "$DATAVISOR_TRIGGER_CONF_DIR/$FILENAME" ]; then
   mv "$DATAVISOR_TRIGGER_CONF_DIR/$FILENAME" "$DATAVISOR_BASE_DIR/data"
   echo "$(date +'%d-%m-%Y %H:%M:%S') - INFO - $task_name - '$DATA_LOC' was found and moved into to SkinnyWMS data dir at '$DATAVISOR_BASE_DIR/data'" >> $DATAVISOR_LOG_PATH
 else
-  echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR - $task_name - '$DATA_LOC' file does not exist. Terminating." >> $DATAVISOR_LOG_PATH
+  echo "$(date +'%d-%m-%Y %H:%M:%S') - ERROR - $task_name - '$DATAVISOR_TRIGGER_CONF_DIR/$FILENAME' file does not exist. Terminating." >> $DATAVISOR_LOG_PATH
   exit 1
 fi
 
